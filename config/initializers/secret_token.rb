856c3db68356f123d6cf5b4b74d96cf37eaf9453
tumura <tumura@zigexn.co.jp>
2013-07-26 15:32:51 +0900

    rails4 new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rendezvous::Application.config.secret_key_base = 'a80405ee8ef36748814987d905a67334cac108f23f233f74cfcb3a86cb57a0cb89aa6f95c8dbf78a853fe2250ad1869d4ca9d1520b730d2589a57a75eb56da10'
