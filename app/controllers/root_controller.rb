f845bb31713b06fabb43cc6f490acba08fe24842
tumura <tumura@zigexn.co.jp>
2013-09-06 21:13:35 +0900

    Topにpostのリスト表示

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,7 @@
 class RootController < ApplicationController
   def index
+    @posts = Post.all.desc.limit(10)
+    @my_posts = Post.having(current_user).desc.limit(10)
     @tags = Post.tag_counts_on(:tags)[0..3]
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ActiveRecord::Base
   acts_as_taggable
   acts_as_taggable_on :users
 
-  scope :by_join_date, -> {order("created_at DESC")}
+  scope :asc, -> {order("created_at ASC")}
+  scope :desc, -> {order("created_at DESC")}
   scope :search_content, ->(q) {where 'content like ?', "%#{q}%"}
   scope :search_tag, ->(q) {Post.tagged_with(q)}
 
